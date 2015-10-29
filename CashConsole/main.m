6b37b397e39e87b882171f1149cee001f7818ab7
popov <mac@macintosh.local>
2015-10-29 21:40:03 +0300

    Initial Commit

---
+++ CashConsole/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CashConsole
+//
+//  Created by popov on 29.10.15.
+//  Copyright © 2015 popov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
