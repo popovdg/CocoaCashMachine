78a955624d19aa1289744241b9675e3a69671f7c
popov <mac@macintosh.local>
2015-11-05 09:29:57 +0300

    Added test with nil values

--- CashMachineTests/CashMachineTests.m
+++ CashMachineTests/CashMachineTests.m
@@ -25,86 +25,104 @@
     [super tearDown];
 }
 
-- (void) testInit {
+- (void) testInit { //Testing setDenominations
     CashMachine* cashMachine = [CashMachine alloc];
     
-    NSArray* values = @[@50, @100, @10, @20]; //Unsorted values
+    //Unsorted values
+    NSArray* values = @[@50, @100, @10, @20];
     NSArray* correctResult = @[@100, @50, @20, @10];
     [cashMachine setDenominations: values];
     NSArray* result = [cashMachine denominations];
     XCTAssertEqualObjects(result, correctResult, @"setDenominations for unsorted values fails");
     
-    values = @[@50, @100, @0, @10, @20]; //With 0$
+    //With 0$
+    values = @[@50, @100, @0, @10, @20];
     correctResult = @[@100, @50, @20, @10];
     [cashMachine setDenominations: values];
     result = [cashMachine denominations];
     XCTAssertEqualObjects(result, correctResult, @"setDenominations for values with 0 fails");
     
-    values = @[@50, @100, @20, @10, @20]; //With duplicates
+    //With duplicates
+    values = @[@50, @100, @20, @10, @20];
     correctResult = @[@100, @50, @20, @10];
     [cashMachine setDenominations: values];
     result = [cashMachine denominations];
     XCTAssertEqualObjects(result, correctResult, @"setDenominations for values with duplicates fails");
     
-    values = @[@50, @100, @"test", @10, @20]; //With string
+    //With string
+    values = @[@50, @100, @"test", @10, @20];
     correctResult = @[@100, @50, @20, @10];
     [cashMachine setDenominations: values];
     result = [cashMachine denominations];
     XCTAssertEqualObjects(result, correctResult, @"setDenominations for values with string fails");
 }
 
-- (void) testSum {
+- (void) testSum { //Testing sum
     CashMachine* cashMachine = [CashMachine alloc];
     
-    NSArray* values = @[@50, @100, @10, @20]; //Unsorted values
+    //Unsorted values
+    NSArray* values = @[@50, @100, @10, @20];
     [cashMachine setDenominations: values];
     int sum = [cashMachine sum: @[@1, @0, @10, @1]];
     XCTAssertEqual(sum, 310);
     
-    values = @[@50, @100, @0, @10, @20]; //With 0$
+    //With 0$
+    values = @[@50, @100, @0, @10, @20];
     [cashMachine setDenominations: values];
     sum = [cashMachine sum: @[@1, @0, @10, @1]];
     XCTAssertEqual(sum, 310);
     
-    values = @[@50, @100, @20, @10, @20]; //With duplicates
+    //With duplicates
+    values = @[@50, @100, @20, @10, @20];
     [cashMachine setDenominations: values];
     sum = [cashMachine sum: @[@1, @0, @10, @1]];
     XCTAssertEqual(sum, 310);
     
-    values = @[@50, @100, @"test", @10, @20]; //With string
+    //With string
+    values = @[@50, @100, @"test", @10, @20];
     [cashMachine setDenominations: values];
     sum = [cashMachine sum: @[@1, @0, @10, @1]];
     XCTAssertEqual(sum, 310);
 }
 
-- (void)testExamples {
+- (void)testExamples { //Testing getCash
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
     CashMachine* cashMachine = [CashMachine alloc];
 
-    NSArray* values = @[@50, @100, @10, @20]; //Unsorted values
+    //Unsorted values
+    NSArray* values = @[@50, @100, @10, @20];
     [cashMachine setDenominations: values];
     NSArray* result = [cashMachine getCash: 50];
     NSArray* correctResult = @[@0, @0, @2, @1];
     XCTAssertEqualObjects(result, correctResult, @"Bad result of ejecting 50$ for unsorted values");
     
-    values = @[@50, @100, @0, @10, @20]; //With 0$
+    //With 0$
+    values = @[@50, @100, @0, @10, @20];
     result = [cashMachine getCash: 10];
     correctResult = @[@0, @0, @0, @1];
     XCTAssertEqualObjects(result, correctResult, @"Bad result of ejecting 10$ for values with 0");
     
-    values = @[@50, @100, @20, @10, @20]; //With duplicates
+    //With duplicates
+    values = @[@50, @100, @20, @10, @20];
     result = [cashMachine getCash: 20];
     correctResult = @[@0, @0, @1, @0];
     XCTAssertEqualObjects(result, correctResult, @"Bad result of ejecting 20$ for duplicated values");
     
-    values = @[@50, @100, @"test", @10, @20]; //With string
+    //With string
+    values = @[@50, @100, @"test", @10, @20];
     result = [cashMachine getCash: 10];
     correctResult = @[@0, @0, @0, @1];
     XCTAssertEqualObjects(result, correctResult, @"Bad result of ejecting 10$ for values with string");
     
-    values = @[@100, @500, @20, @10]; //Incorrect sum
+    //Incorrect sum
+    values = @[@100, @500, @20, @10];
     result = [cashMachine getCash: 1];
     XCTAssertNil(result, @"Bad result of ejecting incorrect sum");
+    
+    //With nil
+    values = nil;
+    result = [cashMachine getCash: 1];
+    XCTAssertNil(result, @"Bad result of testing with nil values");
 }
 @end
